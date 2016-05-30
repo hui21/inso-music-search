6a54d39a13474bdb72a9e2f03ac2b685c02d6825
sysorem <onlyweloving@gmail.com>
2016-05-30 18:33:33 +0800

    inso聚合音乐搜索

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inso - 聚合音乐搜素引擎 | Free Music</title>
+    <link rel="stylesheet" type="text/css" href="/resources/views/vendor/css/style.css">
+</head>
+
+<body>
+<header id="top">
+    <img id="logo">
+    <div>
+        <input id="keyword" type="text" placeholder="输入歌曲名称"></input>
+        <input id="btn_search" type="submit" placeholder="搜索" value="搜索"></input>
+    </div>
+</header>
+
+<footer id="bottom">
+    <p>© 2016 Powered by sysorem , Hdu Geek</p>
+</footer>
+<script type="text/javascript" src="/resources/views/vendor/js/jquery.min.js"></script>
+<script type="text/javascript">
+    $('#btn_search').click(function () {
+        $.ajax();
+    });
+
+
+</script>
+</body>
+</html>
